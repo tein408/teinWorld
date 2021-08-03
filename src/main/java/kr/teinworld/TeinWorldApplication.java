5ca45c98f978f1b6df5bf4bd2745850add001cb6
aria22408 <tein408@gmail.com>
2021-08-03 21:49:19 +0900

    feat:프로젝트 초기 설정

---
+++ src/main/java/kr/teinworld/TeinWorldApplication.java
@@ -0,0 +1,13 @@
+package kr.teinworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeinWorldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeinWorldApplication.class, args);
+    }
+
+}
