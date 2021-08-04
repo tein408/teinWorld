bdd4aaa77460a3f1fdb7102e0d2190391a0a6122
tein408 <tein408@gmail.com>
2021-08-04 16:14:09 +0900

    feat: 로그인 get 요청 처리

---
+++ src/main/java/kr/teinworld/controller/MemberController.java
@@ -0,0 +1,16 @@
+package kr.teinworld.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MemberController {
+
+    @GetMapping(value = "/members/signUp")
+    public String signUp(){
+        return "/members/signUp";
+    }
+
+
+
+}
