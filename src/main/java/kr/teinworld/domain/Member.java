f7e7d6a988cea42e66839c2037ee831f12bdb855
aria22408 <tein408@gmail.com>
2021-08-04 10:41:44 +0900

    feat:권한 컬럼 추가

--- src/main/java/kr/teinworld/domain/Member.java
+++ src/main/java/kr/teinworld/domain/Member.java
@@ -2,6 +2,7 @@ package kr.teinworld.domain;
 
 import lombok.Getter;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -10,11 +11,17 @@ import javax.persistence.Id;
 public class Member {
 
     @Id @GeneratedValue
+    @Column(name = "member_id")
     private Long id;
 
     private String email;
+
+    @Column(length = 15)
     private String name;
+
     private String pwd;
+
+    private String auth;
 
 
 
