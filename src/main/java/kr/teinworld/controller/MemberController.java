9e84cc269cfe49efdec0d2bf5446dafa9da377ef
tein408 <tein408@gmail.com>
2021-08-05 17:55:43 +0900

    feat: 회원 가입 get, post 요청 처리
    - thymleaf th:object를 이용하여 값을 전달하므로
    - MemberForm class를 model에 추가하여 get 요청 처리
    - post요청시 회원 정보를 MemberService에 전달

--- src/main/java/kr/teinworld/controller/MemberController.java
+++ src/main/java/kr/teinworld/controller/MemberController.java
@@ -1,10 +1,22 @@
 package kr.teinworld.controller;
 
+import kr.teinworld.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @Controller
+@RequiredArgsConstructor
+@Slf4j
 public class MemberController {
+
+    private final MemberService memberService;
 
     @GetMapping(value = "/members/signUp")
     public String signUp(){
@@ -12,8 +24,21 @@ public class MemberController {
     }
 
     @GetMapping(value = "/members/new")
-    public String createMember(){
+    public String createMember(Model model){
+        model.addAttribute("memberForm", new MemberForm());
         return "/members/createMemberForm";
+    }
+
+    @PostMapping(value = "/members/new")
+    public String createMember(@Valid MemberForm memberForm, BindingResult result) {
+        //BindingResult -> name field에 대해 에러를 뽑아준다.
+        if(result.hasErrors()){
+            return "/members/createMemberForm";
+        }
+
+        memberService.save(memberForm);
+
+        return "/members/signUp";
     }
 
 
