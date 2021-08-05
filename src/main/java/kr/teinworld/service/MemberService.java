22536a909c7f766075bbe6aea39421e6fd05fa5b
tein408 <tein408@gmail.com>
2021-08-05 18:12:45 +0900

    fix: 비밀번호, 권한 설정 코드 수정
    - MemberForm에서 @Setter 삭제
    - setter 사용시 여러 곳에서 변수 호출 가능하므로
    - setter를 사용하지 않고 값을 설정하여 전달하도록 수정

--- src/main/java/kr/teinworld/service/MemberService.java
+++ src/main/java/kr/teinworld/service/MemberService.java
@@ -4,6 +4,9 @@ import kr.teinworld.controller.MemberForm;
 import kr.teinworld.domain.Member;
 import kr.teinworld.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,10 +22,10 @@ public class MemberService {
     public Long save(MemberForm memberForm){
 
         BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-        memberForm.setPwd(encoder.encode(memberForm.getPwd()));
-        memberForm.setAuth("USER");
+        String pwd = encoder.encode(memberForm.getPwd());
+        String auth = "USER";
 
-        Member member = new Member(memberForm.getEmail(), memberForm.getName(), memberForm.getPwd(), memberForm.getAuth());
+        Member member = new Member(memberForm.getEmail(), memberForm.getName(), pwd, auth);
 
         return memberRepository.save(member);
     }
