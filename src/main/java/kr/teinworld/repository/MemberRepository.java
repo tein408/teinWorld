9893d55cd04d35c21e4e99ba8c880720918904ed
tein408 <tein408@gmail.com>
2021-08-05 17:16:40 +0900

    feat: 회원 저장 기능 추가

---
+++ src/main/java/kr/teinworld/repository/MemberRepository.java
@@ -0,0 +1,20 @@
+package kr.teinworld.repository;
+
+import kr.teinworld.domain.Member;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+
+@Repository
+@RequiredArgsConstructor
+public class MemberRepository {
+
+    private final EntityManager em;
+
+    public Long save(Member member){
+        em.persist(member);
+        return member.getId();
+    }
+
+}
