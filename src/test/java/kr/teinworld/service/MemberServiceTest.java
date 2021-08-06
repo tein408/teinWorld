73acb56f7e7574489ef218a77912b99d47ec7e3a
tein408 <tein408@gmail.com>
2021-08-06 20:11:18 +0900

    test: 이메일 검색 기능 테스트

--- src/test/java/kr/teinworld/service/MemberServiceTest.java
+++ src/test/java/kr/teinworld/service/MemberServiceTest.java
@@ -28,4 +28,19 @@ class MemberServiceTest {
         assertEquals(member.getName(), memberRepository.findOne(saveId).getName());
     }
 
+    @Test
+    public void findByEmail() throws Exception{
+        //given
+        MemberForm member1 = new MemberForm("kim1", "testEmail1@email.com", "password1", "USER");
+        MemberForm member2 = new MemberForm("kim2", "testEmail2@email.com", "password2", "USER");
+
+        //when
+        memberService.save(member1);
+        memberService.save(member2);
+        String email = "testEmail1@email.com";
+
+        //then
+        assertEquals(member1.getEmail(), memberRepository.findByEmail(email).getEmail());
+    }
+
 }
