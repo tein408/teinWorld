6a219e2cba2230a876c921d9848a44b7cd456859
tein408 <tein408@gmail.com>
2021-08-06 12:13:59 +0900

    feat: 로그인 인증 처리 위한 오버라이딩

--- src/main/java/kr/teinworld/config/WebSecurityConfig.java
+++ src/main/java/kr/teinworld/config/WebSecurityConfig.java
@@ -1,14 +1,21 @@
 package kr.teinworld.config;
 
+import kr.teinworld.service.MemberService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final MemberService memberService;
 
     @Override
     public void configure(WebSecurity web) throws Exception {
@@ -20,12 +27,24 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http.authorizeRequests()
                 .antMatchers("/", "home").permitAll()
                 .antMatchers("/members/new").permitAll()
+                .antMatchers("/members/signUp").permitAll()
                 .anyRequest().authenticated()
             .and()
                 .formLogin()
                 .loginPage("/members/signUp").permitAll()
                 .usernameParameter("email")
                 .passwordParameter("pwd")
+                .defaultSuccessUrl("/")
         ;
     }
+
+    //AuthenticationManagerBuilder : AuthenticationManager 생성하는 class
+    // * AuthenticationManager : Spring Security의 모든 인증 관리 interface
+    //AuthenticationManagerBuilder가 UserDetailService를 통해 유저 정보를 memberService에서 찾아 담음
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(memberService)
+                .passwordEncoder(new BCryptPasswordEncoder());
+    }
+
 }
