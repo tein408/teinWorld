d4e10eb21d5f7c6c890f1c85ce0b46955f8d9f90
tein408 <tein408@gmail.com>
2021-08-06 20:21:37 +0900

    feat: 로그인시 로그인한 회원 이름 출력

--- src/main/java/kr/teinworld/controller/HomeController.java
+++ src/main/java/kr/teinworld/controller/HomeController.java
@@ -1,13 +1,33 @@
 package kr.teinworld.controller;
 
+import kr.teinworld.domain.Member;
+import kr.teinworld.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
+
+    private final MemberRepository memberRepository;
 
     @RequestMapping("/")
     public String home() {
+        return "home";
+    }
+
+    @GetMapping("/")
+    public String home(@AuthenticationPrincipal Member member, Model model){
+        if(member != null){
+            Member findMember = memberRepository.findOne(member.getId());
+            if(findMember != null) {
+                model.addAttribute("member", findMember.getName());
+            }
+        }
         return "home";
     }
 
