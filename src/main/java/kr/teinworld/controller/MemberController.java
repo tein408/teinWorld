60c8357ada4a564f0f4dc78a9dbe2f2817637436
tein408 <tein408@gmail.com>
2021-08-09 21:51:13 +0900

    feat: 이메일 중복 체크 요청 처리
    - ajax로 회원 가입 전 이메일 중복 체크 위해 작성

--- src/main/java/kr/teinworld/controller/MemberController.java
+++ src/main/java/kr/teinworld/controller/MemberController.java
@@ -1,15 +1,16 @@
 package kr.teinworld.controller;
 
+import kr.teinworld.domain.Member;
 import kr.teinworld.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.Optional;
 
 @Controller
 @RequiredArgsConstructor
@@ -29,15 +30,26 @@ public class MemberController {
         return "/members/createMemberForm";
     }
 
+    @ResponseBody
+    @RequestMapping(value = "members/dupEmail")
+    public String dupEmail (@RequestBody MemberForm memberForm) {
+        String message;
+        Optional<Member> findEmail = memberService.findByEmail(memberForm.getEmail());
+        if(findEmail.isPresent()) {
+            message = "이미 사용중인 이메일";
+        } else {
+            message = "사용 가능한 이메일";
+        }
+        return message;
+    }
+
     @PostMapping(value = "/members/new")
     public String createMember(@Valid MemberForm memberForm, BindingResult result) {
         //BindingResult -> name field에 대해 에러를 뽑아준다.
         if(result.hasErrors()){
             return "/members/createMemberForm";
         }
-
         memberService.save(memberForm);
-
         return "/members/signUp";
     }
 
