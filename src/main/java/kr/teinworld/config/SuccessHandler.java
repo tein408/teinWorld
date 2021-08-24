42459d90967af40cd747da19a0d00ee9c7e255b9
tein408 <tein408@gmail.com>
2021-08-24 20:07:41 +0900

    feat: SuccessHandler 추가
    - 로그인 성공시 session에 Member entity 추가하기 위함

---
+++ src/main/java/kr/teinworld/config/SuccessHandler.java
@@ -0,0 +1,34 @@
+package kr.teinworld.config;
+
+import kr.teinworld.domain.Member;
+import kr.teinworld.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class SuccessHandler implements AuthenticationSuccessHandler {
+
+    private final MemberService memberService;
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+
+        HttpSession session = request.getSession();
+
+        Member member = memberService.findByEmail(authentication.getName()).get();
+
+        session.setAttribute("member", member);
+
+        response.sendRedirect("/");
+
+    }
+}
